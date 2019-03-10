ba751490d88396512fbd2d19f56e8c95f9306064
adem çağlın <adem.caglin@gmail.com>
2019-03-10 19:43:38 +0300

    updated certificate storage

--- ipfs-ethereum/contracts/CertificateStorage.sol
+++ ipfs-ethereum/contracts/CertificateStorage.sol
@@ -1,24 +1,19 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract CertificateStorage {
-      
-    struct IssuerAuthority {
-        uint created_at;
-        string title; 
-    }
 
     struct Issuer {
         uint created_at;
         uint disabled_at; 
         string title; 
-        address authority;
+        address admin;
     }
   
     struct Signer {
         uint created_at;
         uint disabled_at; 
         string title; 
-        address issuer;
+        bytes32 issuer;
     }
 
     struct Certificate {
@@ -26,32 +21,66 @@ contract CertificateStorage {
         uint created_at; 
         uint disabled_at;  
         bytes32 ipfs_hash; 
-        bytes2 ipfs_alg;
     }
-  
-    mapping(bytes32 => Certificate)  public certificates;
-    mapping(address => Signer)  public signers;
-    mapping(address => Issuer)  public issuers;
-    mapping(address => IssuerAuthority)  public issuerAuthorities;
 
-    function createCertificate(bytes32 id, bytes2 ipfs_alg, bytes32 ipfs_hash) public {
+    address public owner;
+    mapping(bytes32 => Certificate) public certificates;
+    mapping(address => Signer) public signers;
+    mapping(bytes32 => Issuer) public issuers;
+
+    constructor() public {
+        owner = msg.sender;
+        bytes32 issuerId = stringToBytes32("DECENTRALIZED_ISSUER");
+        
+        Issuer storage issuer = issuers[issuerId];
+        issuer.title = "Decentralized Certificate Issuer";
+        issuer.created_at = now;
+        issuer.admin = msg.sender;
+
+        Signer storage signer = signers[msg.sender];
+        signer.title = "Owner";
+        signer.created_at = now;
+        signer.issuer = issuerId;
+    }
+
+    function createCertificate(bytes32 id, bytes32 ipfs_hash, uint created_at) public {
+        require((now > (created_at + 1 days)) || (now < created_at), "The created_at is not valid.");
+        Signer memory signer = signers[msg.sender];
+        require(signer.issuer == stringToBytes32("DECENTRALIZED_ISSUER"), "The signer issuer should be ss");
+        require(signer.created_at != 0, "The signer created_at is zero");
+        require(signer.disabled_at == 0, "The account is not valid signer");
+        Issuer memory issuer = issuers[signer.issuer];
+        require(issuer.created_at != 0, "The signer has not a valid issuer");
+        require(issuer.disabled_at == 0, "The signer has not a valid issuer");
         Certificate storage cer = certificates[id];
         cer.signer = msg.sender;
-        cer.created_at = now;
+        cer.created_at = created_at;
         cer.ipfs_hash = ipfs_hash;
-        cer.ipfs_alg = ipfs_alg;
     }
   
-    function getCertificate(bytes32 id)public view returns(address,uint,bytes32,string memory,string memory,string memory){
+    function getCertificate(bytes32 id)public view returns(address,uint,bytes32,string memory,string memory){
         Certificate memory cer = certificates[id];
         Signer memory signer = signers[cer.signer];
-        Issuer storage issuer = issuers[signer.issuer];
-        IssuerAuthority storage authority = issuerAuthorities[issuer.authority];
+        Issuer memory issuer = issuers[signer.issuer];
         return(cer.signer,
         cer.created_at,
         cer.ipfs_hash,
         signer.title,
-        issuer.title,
-        authority.title);
+        issuer.title);
     }
+
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+           return 0x0;
+        }
+        assembly {
+           result := mload(add(source, 32))
+        }
+    }
+    /* 
+     * createIssuer()
+     * disableCertificate()
+     * disableIssuer()
+     */
 }
--- ipfs-ethereum/test/TestCertificateStorage.sol
+++ ipfs-ethereum/test/TestCertificateStorage.sol
@@ -5,38 +5,105 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/CertificateStorage.sol";
 
 contract TestCertificateStorage {
+   function testOwnerShouldBeCreated() public {
+       CertificateStorage certificateStorage = CertificateStorage(DeployedAddresses.CertificateStorage());
+       Assert.equal(certificateStorage.owner(), msg.sender, "An owner is different than a deployer");
+   }
 
-  function testItStoresAValue() public {
-    CertificateStorage certificateStorage = CertificateStorage(DeployedAddresses.CertificateStorage());
-    bytes32 id = stringToBytes32("Zz8svcnkgxuRxS3c8tb7BLEdrNQ9BCLmahFAgrtrWnEp");
-    certificateStorage.createCertificate(id, stringToBytes2('Qm'), stringToBytes32('Zz8svcnkgxuRxS3c8tb7BLEdrNQ9BCLmahFAgrtrWnEp'));
-    
-    address addr;
-    uint created_at;
-    bytes32 ipfs_hash;
-    bytes2 ipfs_alg;
-    (addr, created_at, ipfs_hash, ipfs_alg) = certificateStorage.getCertificate(id);
-    Assert.equal(ipfs_hash, stringToBytes32("Zz8svcnkgxuRxS3c8tb7BLEdrNQ9BCLmahFAgrtrWnEp"), "It should store the value 89.");
-  }
+   function testSignerShouldBeCreated() public {
+      CertificateStorage certificateStorage = CertificateStorage(DeployedAddresses.CertificateStorage());
+      string memory title;
+      bytes32 issuer;
+      uint created_at;
+      uint disabled_at;
+      (created_at, disabled_at, title, issuer) = certificateStorage.signers(certificateStorage.owner());
+      string memory expected = "Owner";
+      Assert.isNotZero(created_at, "The signer created_at is zero");
+      Assert.equal(title, expected, "The signer is not created");
+   }
+   
+   function testIssuerShouldBeCreated() public {
+      CertificateStorage certificateStorage = CertificateStorage(DeployedAddresses.CertificateStorage());
+      bytes32 issuerId = stringToBytes32("DECENTRALIZED_ISSUER");
+      string memory title;
+      bytes32 authority;
+      uint created_at;
+      uint disabled_at;
+      address admin;
+      (created_at, disabled_at, title, authority, admin) = certificateStorage.issuers(issuerId);
+      string memory expected = "Decentralized Certificate Issuer";
+      Assert.isNotZero(created_at, "The created_at is zero");
+      Assert.equal(title, expected, "The issuer is not created");
+      Assert.equal(admin, msg.sender, "The issuer's admin is not created");
+   }
 
-function stringToBytes32(string memory source) public returns (bytes32 result) {
-    bytes memory tempEmptyStringTest = bytes(source);
-    if (tempEmptyStringTest.length == 0) {
-        return 0x0;
+   function testCertificateShouldBeCreated() public {
+      CertificateStorage certificateStorage = CertificateStorage(DeployedAddresses.CertificateStorage());
+      address signer;
+      uint created_at; 
+      uint disabled_at;  
+      bytes32 ipfs_hash; 
+      bytes32 id = stringToBytes32("Zz8svcnkgxuRxS3c8tb7BLEdrNQ9BCLmahFAgrtrWnEp");
+      //certificateStorage.createCertificate(id, id, now);
+      (signer,created_at,disabled_at,ipfs_hash)=certificateStorage.certificates(id);
+      Assert.isNotZero(created_at, 
+        "created_at");
+      Assert.equal(signer, certificateStorage.owner(), 
+        "It should address equals msg.sender");
+   }
+   
+   function testGetCertificate() public {
+      CertificateStorage certificateStorage = CertificateStorage(DeployedAddresses.CertificateStorage());
+      bytes32 id = stringToBytes32("Zz8svcnkgxuRxS3c8tb7BLEdrNQ9BCLmahFAgrtrWnEp");
+      certificateStorage.createCertificate(id, id, now);
+      address addr;
+      uint created_at;
+      bytes32 ipfs_hash;
+      string memory signer_title;
+      string memory issuer_title;
+      string memory authority_title;
+      (addr, created_at, ipfs_hash, signer_title, issuer_title, authority_title) = certificateStorage.getCertificate(id);
+      string memory expected_signer_title = "Owner";
+      Assert.isNotZero(created_at, 
+        "created_at");
+      Assert.equal(ipfs_hash, stringToBytes32("Zz8svcnkgxuRxS3c8tb7BLEdrNQ9BCLmahFAgrtrWnEp"), 
+        "It should store the value Zz8svcnkgxuRxS3c8tb7BLEdrNQ9BCLmahFAgrtrWnEp. ");
+      Assert.equal(addr, msg.sender, 
+        "It should address equals msg.sender");
+      Assert.equal(signer_title, expected_signer_title, 
+        "It should store signer_title");
+      /*Assert.equal(issuer_title, "", 
+        "It should store issuer_title");
+      Assert.equal(authority_title, "", 
+        "It should store authority_title");*/
+   }
+
+   function stringToBytes32(string memory source) public returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
     }
 
-    assembly {
-        result := mload(add(source, 32))
-    }
-}
-function stringToBytes2(string memory source) public returns (bytes2 result) {
-    bytes memory tempEmptyStringTest = bytes(source);
-    if (tempEmptyStringTest.length == 0) {
-        return 0x0;
+    function bytes32ToString(bytes32 x) public returns (string memory) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (uint j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
     }
 
-    assembly {
-        result := mload(add(source, 2))
-    }
-}
 }
